b2676ecf4a259e72f7bc89fa514a8ff63762026f
notwillcasey <notwillcasey@gmail.com>
2024-01-05 23:21:16 -0700

    update calls to github api

--- Controllers/UserReposController.cs
+++ Controllers/UserReposController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace github_fetcher_api_dotnet.Controllers;
 
@@ -17,21 +18,21 @@ public class UserReposController : ControllerBase
         public async Task<IActionResult> GetUserRepos(string user)
         {
                 var apiUrl = $"https://api.github.com/users/{user}/repos";
-                using (var request = new HttpRequestMessage(HttpMethod.Get, apiUrl))
+                _httpClient.DefaultRequestHeaders.Add("Accept", "application/vnd.github+json");
+                _httpClient.DefaultRequestHeaders.Add("User-Agent", "Request");
+                _httpClient.DefaultRequestHeaders.Add("token", "TOKEN");
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+                
+                if (response.IsSuccessStatusCode)
                 {
-                        request.Headers.Accept.TryParseAdd("application/vnd.github+json");
-                        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
-                        
-                        if (response.IsSuccessStatusCode)
-                        {
-                                string data = await response.Content.ReadAsStringAsync();
-                                return Ok(data);
-                        }
-                        else
-                        {
-                                return StatusCode((int)response.StatusCode);
-                        }
+                        string data = await response.Content.ReadAsStringAsync();
+                        return Ok(data);
                 }
+                else
+                {
+                        return StatusCode((int)response.StatusCode);
+                }
+
 
 
                 
